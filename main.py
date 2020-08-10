40f43e7e00a366ce1e9eb3934d1bd03de55fb416
mariliafernandez <marilia.mfernandez@gmail.com>
2020-08-09 23:36:17 -0300

    Random Forest Classifier added and 2020 prediction

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import numpy as np
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import classification_report
 from sklearn import preprocessing
-from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 
 def class_size(y,label):
     return len(y[y == label])
@@ -18,18 +18,14 @@ def class_size(y,label):
 def resample(X, y):
     print('\nOriginal')
     for i in range( len(y.unique()) ): print( str(i) + ': ' + str(class_size(y,i)) )
-
     # under = RandomUnderSampler( sampling_strategy = 'majority' )
     under = RepeatedEditedNearestNeighbours( sampling_strategy = 'majority' )
     X_res, y_res = under.fit_resample(X, np.ravel(y, order='C'))
-
     over = ADASYN( sampling_strategy={0:class_size(y_res,0), 1:int(class_size(y_res,0)*0.7), 2:int(class_size(y_res,0)*0.7)} ) 
     # over = ADASYN( sampling_strategy='not majority' ) 
     X_res, y_res = over.fit_resample(X_res, np.ravel(y_res, order='C'))
-
     print('\nResampled')
     for i in range( len(y.unique()) ): print( str(i) + ': ' + str(class_size(y_res,i)) )
-
     return X_res, y_res
 
 def print_metrics(pred, y_test):
@@ -53,80 +49,68 @@ def search_parameters(clf, distributions, X,y):
     search = grid.fit(X,y)
     print(search.best_params_)
 
-
-data = pd.read_csv('Safra_2018-2019.csv')
-
-# -----------------------------------Preprocessing------------------------------------------
 # remove rows with null data
-data = data.dropna(subset=['Semanas_Utilizando'])
+def remove_null(data):
+    for column in data.columns:
+        data = data.dropna(subset=[ column ])
+    return data
 
 # expand categorical attributes into columns
-data['Cultivo_1'] = data['Tipo_de_Cultivo']
-data['Cultivo_2'] = 0
-data.loc[data['Cultivo_1'] == 0, 'Cultivo_2'] = 1
+def transform_categorical(df):
+    data = df
+    data['Cultivo_1'] = data['Tipo_de_Cultivo']
+    data['Cultivo_2'] = 0
+    data.loc[data['Cultivo_1'] == 0, 'Cultivo_2'] = 1
+    data['Solo_1'] = data['Tipo_de_Solo']
+    data['Solo_2'] = 0
+    data.loc[data['Solo_1'] == 0, 'Solo_2'] = 1
+    data['Pesticida_1']=0
+    data['Pesticida_2']=0
+    data['Pesticida_3']=0
+    data.loc[data['Categoria_Pesticida'] == 1, 'Pesticida_1'] = 1
+    data.loc[data['Categoria_Pesticida'] == 2, 'Pesticida_2'] = 1
+    data.loc[data['Categoria_Pesticida'] == 3, 'Pesticida_3'] = 1
+    data['Temporada_1']=0
+    data['Temporada_2']=0
+    data['Temporada_3']=0
+    data.loc[data['Temporada'] == 1, 'Temporada_1'] = 1
+    data.loc[data['Temporada'] == 2, 'Temporada_2'] = 1
+    data.loc[data['Temporada'] == 3, 'Temporada_3'] = 1
+    data = data.drop( ['Unnamed: 0', 'Identificador_Agricultor', 'Tipo_de_Cultivo', 'Tipo_de_Solo', 'Categoria_Pesticida', 'Temporada'], axis=1 )
+    return data
 
-data['Solo_1'] = data['Tipo_de_Solo']
-data['Solo_2'] = 0
-data.loc[data['Solo_1'] == 0, 'Solo_2'] = 1
+# normalize sample values between 0-1
+def rescale(data):
+    min_max = preprocessing.MinMaxScaler()
+    scaled = min_max.fit_transform(X)
+    return pd.DataFrame(scaled)
 
-data['Pesticida_1']=0
-data['Pesticida_2']=0
-data['Pesticida_3']=0
-data.loc[data['Categoria_Pesticida'] == 1, 'Pesticida_1'] = 1
-data.loc[data['Categoria_Pesticida'] == 2, 'Pesticida_2'] = 1
-data.loc[data['Categoria_Pesticida'] == 3, 'Pesticida_3'] = 1
-
-data['Temporada_1']=0
-data['Temporada_2']=0
-data['Temporada_3']=0
-data.loc[data['Temporada'] == 1, 'Temporada_1'] = 1
-data.loc[data['Temporada'] == 2, 'Temporada_2'] = 1
-data.loc[data['Temporada'] == 3, 'Temporada_3'] = 1
-
-# merge target minority classes into one 
-data['tem_dano']=0
-data.loc[data['dano_na_plantacao'] == 0, 'tem_dano'] = 0 
-data.loc[data['dano_na_plantacao'] == 1, 'tem_dano'] = 1
-data.loc[data['dano_na_plantacao'] == 2, 'tem_dano'] = 1 
-
-data['dano_outros']=0
-data.loc[data['dano_na_plantacao'] == 1, 'dano_outros'] = 1 
-
-data['dano_por_pesticida']=0
-data.loc[data['dano_na_plantacao'] == 2, 'dano_por_pesticida'] = 1 
-
+data = pd.read_csv('Safra_2018-2019.csv')
+data = remove_null(data)
+data = transform_categorical(data)
 
 # extract sample and target
 y = data['dano_na_plantacao']
-X = data.drop(['Unnamed: 0', 'Identificador_Agricultor', 'dano_na_plantacao', 'Tipo_de_Cultivo', 'Tipo_de_Solo', 'Categoria_Pesticida', 'Temporada', 'tem_dano', 'dano_outros', 'dano_por_pesticida'], axis=1)
-
-# normalize data
-min_max = preprocessing.MinMaxScaler()
-scaled = min_max.fit_transform(X)
-X = pd.DataFrame(scaled)
+X = data.drop(['dano_na_plantacao'], axis=1)
 
 # split data into train(66%) and test(33%) 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 
 X_train, y_train = resample(X_train, y_train)
 
-distributions = {
-    'C':[1],
-    'multi_class': ['ovr'],
-    'class_weight':[None],
-    'dual':[False],
-    'max_iter': [3000]
-}
-# search_parameters(LinearSVC(), distributions, X_train, y_train)
-
-# ------------------------------------------------Classifier------------------------
-# clf = RandomForestClassifier(class_weight='balanced', n_estimators=50, max_features='log2', max_depth=30, bootstrap=False)
-# clf = KNeighborsClassifier(n_neighbors=2, weights='distance', p=2)
-clf = LinearSVC(C=1,class_weight='balanced', dual=False, max_iter=5000)
+# classifier
+clf = RandomForestClassifier(class_weight='balanced', n_estimators=50, max_features='log2', max_depth=30, bootstrap=False)
 
 clf.fit(X_train, y_train)
-pred = clf.predict(X_test)
-print_metrics(pred, y_test)
 
+# prediction
+data_2020 = pd.read_csv( 'Safra_2020.csv' )
+data_2020 = remove_null(data_2020)
+data_2020 = transform_categorical(data_2020)
 
+pred = clf.predict(data_2020)
 
+result = pd.read_csv( 'Safra_2020.csv' )
+result = remove_null(result)
+result['dano_na_plantacao'] = pred
+result.to_csv( 'Predict_Safra_2020.csv' )
